5d636e8bdda289e6fbd41811fbac0b67af8ea4ee
Sai Katterishetty <rao.saikrishna@gmail.com>
2024-08-17 22:48:05 -0500

    Added logger

---
+++ README.md
@@ -0,0 +1,19 @@
+### API responsible for Saving Notes
+
+
+Attributes Captured
+
+- Title
+- Description
+- Date
+- Status
+  - Active
+  - InActive
+
+
+### DB_TABLE: APP_USER.T_USER_NOTES
+Columns:
+- ID
+- DESCRIPTION
+- CREATED_AT
+- STATUS
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/saikrir/keep-notes/internal/logger"
+)
+
+func Run() error {
+	logger.Info("Hello", "World")
+	return nil
+}
 
 func main() {
+	Run()
 	fmt.Println("Hello World")
 }
